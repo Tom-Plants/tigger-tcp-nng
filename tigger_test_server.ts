025aa8ab4cdbb789ef150dceed6401f723ac1853
assembly <assembly0x7c00@gmail.com>
2021-10-28 16:42:49 +0800

    s

--- tigger_test_server.ts
+++ tigger_test_server.ts
@@ -3,7 +3,6 @@ import {socket ,Socket} from "nanomsg";
 let a: Socket = socket("pair");
 
 a.bind("tcp://0.0.0.0:12345");
-a.send(Buffer.alloc(1024* 1024*1024));
 
 a.on("data", (data: Buffer) => {
     console.log(data);
