ce428fad7e55095c494734a89cdd527f8878687d
Assembly <assembly0x7c00@gmail.com>
2021-10-31 04:29:29 +0800

    a

--- server.ts
+++ server.ts
@@ -39,6 +39,7 @@ export default function StartServer(
     server = new Server(host, port, tunnelN);
     server.onDataRecived(onDataRecive);
     server.onDrain(() => {
+        console.log("server >> tunnel continued");
         mapper.forEach((value: Socket) => {
             value.resume();
         });
@@ -69,6 +70,7 @@ function onDataRecive(arg: number, data: Buffer) {
                 server.sendData(Buffer.from("SHALF"), arg);
             }).on("data", (data: Buffer) => {
                 if(false == server.sendData(data, arg)) {
+                    console.log(arg, "server >> tunnel paused");
                     conn.pause();
                 }
             }).on("close", () => {
